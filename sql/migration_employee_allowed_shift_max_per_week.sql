de61ae5f218137b1be4f3d6d72728290d14e1fad
Abid Hussain <abid.hussain@abid76.de>
2026-03-02 10:57:44 +0100

    tinyint

--- sql/migration_employee_allowed_shift_max_per_week.sql
+++ sql/migration_employee_allowed_shift_max_per_week.sql
@@ -2,6 +2,6 @@
 -- Nur ausführen, wenn die Tabelle employee_allowed_shift bereits existiert.
 
 ALTER TABLE employee_allowed_shift
-  ADD COLUMN max_per_week INT UNSIGNED NULL
+  ADD COLUMN max_per_week TINYINT UNSIGNED NULL
   COMMENT 'Max. Anzahl dieser Schicht pro Woche (NULL = unbegrenzt)'
   AFTER shift_id;
